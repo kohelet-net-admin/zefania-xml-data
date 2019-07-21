93dbf86682b8b74989a442b8fb4745e1c718a93c
Kohelet Network Admin <git-master@kohelet-network.org>
2019-07-21 18:47:22 +0200

    Initial commit

---
+++ AssemblyInfo.vb
@@ -0,0 +1,15 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("ZefaniaXml Data")>
+<Assembly: AssemblyDescription("Zefania XML .NET library for object related access")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ZefaniaXml Data")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+<Assembly: Guid("1cfe4433-8b60-48d7-9df4-fd4a7ddc2464")>
+
+<Assembly: AssemblyVersion("2019.07.21.100")>
